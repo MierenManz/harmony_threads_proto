6d15883278a1117f80647413d1c6eeaed7f95008
MierenManz <christiaanmens@gmail.com>
2021-11-26 23:44:37 +0100

    x

---
+++ threadpool.ts
@@ -0,0 +1,34 @@
+import { Allocator } from "./allocator.ts";
+
+class ThreadPool<T, R> {
+    #sabAllocator: Allocator;
+    #workers: Worker[];
+    #internalWriter: WritableStream<R>;
+    #busy: boolean[];
+    constructor(workerURL: URL, count: number) {
+        this.#workers = [];
+        this.#busy = new Array(count).fill(false);
+        this.#internalWriter = new WritableStream();
+        this.#sabAllocator = new Allocator(1024 ** 2 * count);
+        for (let i = 0; i < count; i++) {
+            const worker = new Worker(workerURL.href, { type: "module", name: `harmony_worker${i}` });
+            worker.onmessage = (evt: MessageEvent<R>) => {
+                this.#busy[i] = false;
+                this.#internalWriter.getWriter().write(evt.data);
+            }
+            this.#workers[i] = worker;
+        }
+    }
+
+    process(data: T): void {
+        for (let i = 0; i < this.#workers.length; i++) {
+            if (this.#busy[i] === false) {
+                // Send ptr and length
+                this.#workers[i].postMessage();
+            }
+        }
+    }
+
+    iterator(): void {
+    }
+}
